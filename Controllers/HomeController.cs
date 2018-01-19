a580895ac5ec30418985778d56f72af15d07f39a
Raos <raos.fast@gmail.com>
2018-01-19 03:31:38 -0300

    Documentacion Login
    Se documento el homocontroller donde se explica los metodos que se crearon para la realizacion de un sistema de login y sesion. Ademas de documentar la vista y la condicion que solo muestra el contenido al iniciar sesion.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,6 +17,10 @@ namespace Cotizaciones.Controllers
         {
             _context = context;
         }
+
+        ///Metodo que llama a la vista "Index" donde se tomara el nombre de usuario
+        ///de la session, ademas de generar una lista con todos los usuarios registrados
+        ///para poder mostrarlo a traves de una lista de usuarios llamada "Usuarios Registrados"
         public IActionResult Index()
         {
             ViewBag.NombreUsuario = HttpContext.Session.GetString("NombreUsuario");
@@ -41,45 +45,60 @@ namespace Cotizaciones.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        ///metodo que llama a la vista Register
         public ActionResult Register()
         {
             return View();
         }
 
+        ///metodo que ocurre inmediatamente cuando el usuario le da al boton registrar
+        ///cuando el usuario esta en la vista de registrar usuario
         [HttpPost]
         public ActionResult Register(Usuario user)
         {
+            ///si el estado del modelo es valido se procede a ingresar al usuario a la db
             if(ModelState.IsValid){
+                ///crea el usuario como objeto
                 _context.Usuarios.Add(user);
+                ///guarda el usuario en la base de datos
                 _context.SaveChanges();
 
                 ModelState.Clear();
+                ///envia un mensaje avisando que el usaurio se registro correctamente
                 ViewBag.Message = user.NombreUsuario + "Ha sido registrado exitosamente!";
             }
             return View();
         }
-
+        ///metodo que llama a la vista Login
         public ActionResult Login()
         {
             return View();
         }
 
+        ///metodo que ocurre inmediatamente cuando el usuario le da al boton Ingresar
+        ///cuando el usuario esta en la vista de iniciar sesion
         [HttpPost]
         public ActionResult Login(Usuario user)
         {
+            ///verifica que el nombre de usuario y password que se ingreso este en la base de datos
             var cuenta = _context.Usuarios.Where(u => u.NombreUsuario == user.NombreUsuario && u.Password == user.Password).FirstOrDefault();
+            ///si la cuenta existe y coinciden los datos se agregan estos datos como un contexto http llamado sesion
+            ///que guardara la sesion iniciada durante toda la aplicacion
             if(cuenta != null)
             {
                 HttpContext.Session.SetString("UsuarioID", cuenta.UsuarioID.ToString());
                 HttpContext.Session.SetString("NombreUsuario", cuenta.NombreUsuario);
+                ///redirecciona a la vista index
                 return RedirectToAction("Index");
             }else
             {
+                ///si los datos no existe o no coinciden se manda un mensaje de error
                 ModelState.AddModelError("","Usuario o Password Estan Erroneos");
             }
             return View();
         }
-        
+        ///Este metodo no se utilizo ya que el mensaje de bienvenida se implemente en la vista Index
         public ActionResult Welcome()
         {
             if(HttpContext.Session.GetString("UsuarioID") != null)
@@ -95,8 +114,10 @@ namespace Cotizaciones.Controllers
             
         }
 
+        ///metodo que hace que la sesion ya iniciada se cierre
         public ActionResult Logout()
         {
+            ///esta linea de codigo hace limpiar(clear) la session, borrandola
             HttpContext.Session.Clear();
             return RedirectToAction("Index");
             
